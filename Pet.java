796628bf2b9a7a40a7e6bf695aa9af26f5ff41b7
jhpetrie <150563346+jhpetrie@users.noreply.github.com>
2023-11-12 21:06:23 -0600

    Update Pet.java
    Removed unnecessary id attribute, id will be the index

--- Pet.java
+++ Pet.java
@@ -1,20 +1,10 @@
 public class Pet {
-    private int id = 0;
     private String name;
     private int age;
 
     public Pet(String name, int age) {
-        this.id++;
         this.name = name;
         this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
