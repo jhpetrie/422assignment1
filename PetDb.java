d05178c6651b3cbb3d3ab6d88aa7b85a30ae9daa
jhpetrie <150563346+jhpetrie@users.noreply.github.com>
2023-11-12 21:29:07 -0600

    Update PetDb.java
    add  pet and show pet logic, beginning of main displaying the menu

--- PetDb.java
+++ PetDb.java
@@ -3,4 +3,43 @@ import java.util.Scanner;
 
 public class PetDb {
     private ArrayList<Pet> petList;
+
+    public PetDb() {
+        petList = new ArrayList<>();
+    }
+
+    public void addPet(String name, int age) {
+        petList.add(new Pet(name, age));
+        System.out.println("pet added successfully");
+    }
+
+    public void showPets() {
+        System.out.println("+----------------------+");
+        System.out.printf("| %-3s | %-10s | %-4s |\n", "ID", "NAME", "AGE");
+        System.out.println("+----------------------+");
+        for (int i = 0; i < petList.size(); i++) {
+            Pet pet = petList.get(i);
+            System.out.printf("| %-3d | %-10s | %-4d |\n", i, pet.getName(), pet.getAge());
+        }
+        System.out.println("+----------------------+");
+        System.out.println(petList.size() + " rows in set.");
+    }
+
+    public static void main(String[] args) {
+        PetDb petDb = new PetDb();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pet Database Program\n");
+        while (true) {
+            System.out.println("What would you like to do?");
+            System.out.println("1. View all pets");
+            System.out.println("2. Add more pets");
+            System.out.println("3. Update an existing pet");
+            System.out.println("6. Remove an existing pet");
+            System.out.println("4. Search pets by name");
+            System.out.println("5. Search pets by age");
+            System.out.println("7. Exit Program");
+            System.out.print("Enter your choice: ");
+            int option = scan.nextInt();
+        }
+    }
 }
